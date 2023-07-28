fd218c9a7683616f168839d842e595372022a2ca
iris <iris.lim@thundersoft.com>
2023-07-28 18:18:58 +0900

    0728_18_18: add capture button

--- app/src/main/java/com/example/camerasurface_v3/MainActivity.java
+++ app/src/main/java/com/example/camerasurface_v3/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.camerasurface_v3;
 
+import static android.provider.MediaStore.Images.Media.insertImage;
+
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Camera;
 import android.graphics.ImageFormat;
 import android.hardware.camera2.CameraAccessException;
@@ -10,12 +14,18 @@ import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.ExifInterface;
+import android.media.Image;
 import android.media.ImageReader;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.provider.ContactsContract;
@@ -26,6 +36,7 @@ import android.util.SparseArray;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
@@ -35,11 +46,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
     private static final String TAG = "IRISCameraApp";
 
+    //Camera2 and SurfaceView
     public String CamID = "0";
     private CameraDevice mCameraDevice;
     private CaptureRequest.Builder mCaptReqBuilder;
@@ -47,18 +66,17 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     private SurfaceView mSurfaceView;
     private SurfaceHolder mSurfaceHolder;
 
+    //Save Image
     private ImageReader mImageReader;
+    private File file;
 
     private Handler mHandler;
+    HandlerThread handlerThread;
+    Handler mainHandler ;
     private int mWidth = 0;
     private int mHeight = 0;
 
     private MediaRecorder mMediaRecorder;
-
-
-
-
-
 
     private static final SparseArray ORIENTATIONS = new SparseArray();
     static {
@@ -68,25 +86,43 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         ORIENTATIONS.append(ExifInterface.ORIENTATION_ROTATE_270, 270);
     }
 
-
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG,"iris___onCreate() start");
+        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
 
         setContentView(R.layout.main_activity);
         mSurfaceView = findViewById(R.id.surfaceView);
-        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
-        Log.d(TAG,"iris onCreate2");
 
-        Button capture = findViewById(R.id.captureButton);
-        capture.setAlpha(0.9f);
-
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"iris onResume");
+        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
+        startBackgroundThread();
+        Button buttonCapture = findViewById(R.id.captureButton);
+        buttonCapture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try {
+                    takePicture();
+                } catch (CameraAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
         initSurfaceView();
     }
 
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"iris onPause");
+        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
+        stopBackgroundThread();
+    }
 
     private void initSurfaceView(){
         Log.d(TAG,"iris initSurfaceView");
@@ -97,19 +133,19 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         mHeight=displayMetrics.heightPixels;
 
  */
-
         mSurfaceHolder = mSurfaceView.getHolder();
-
         mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(@NonNull SurfaceHolder holder) {
                 Log.d(TAG,"iris surfaceCreated");
-                initCamAndPrev();
+                //mSurfaceView.setAlpha(0.9f);
+
             }
 
             @Override
             public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
                 Log.d(TAG,"iris surfaceChanged");
+                initCamAndPrev();
             }
 
             @Override
@@ -119,22 +155,42 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         });
     }
 
-    public void initCamAndPrev(){
-        Log.d(TAG,"iris initCamAndPrev");
-        HandlerThread handlerThread = new HandlerThread("Camera2");
+    private void startBackgroundThread(){
+        handlerThread = new HandlerThread("Camera2");
         handlerThread.start();
         mHandler = new Handler(handlerThread.getLooper());
+        mainHandler= new Handler(getMainLooper());
+    }
+    private void stopBackgroundThread(){
+        handlerThread.quitSafely();
+        try {
+            handlerThread.join();
+            handlerThread=null;
+            mHandler=null;
+            mainHandler=null;
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void initCamAndPrev(){
+        Log.d(TAG,"iris initCamAndPrev");
 
         openCamera();
     }
 
     private void openCamera() {
         Log.d(TAG,"iris openCamera");
+
         CameraManager mCameraManager= (CameraManager) this.getSystemService(Context.CAMERA_SERVICE);
         try {
             CameraCharacteristics characteristics = mCameraManager.getCameraCharacteristics(CamID);
             StreamConfigurationMap scm = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
             Size mPreviewSize = scm.getOutputSizes(ImageFormat.JPEG)[0];
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) return;
+            mCameraManager.openCamera(CamID,deviceStateCallback, mHandler);
+
             //setAspectRatioView(mPreviewSize.getHeight(),mPreviewSize.getWidth());
 
             mImageReader = ImageReader.newInstance(
@@ -142,11 +198,7 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                     mPreviewSize.getHeight(),
                     ImageFormat.JPEG,
                     7);
-
-            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                    != PackageManager.PERMISSION_GRANTED) return;
-            mCameraManager.openCamera(CamID,deviceStateCallback, mHandler);
-
+            mImageReader.setOnImageAvailableListener(mOnImageAvailableListener,mainHandler);
         } catch (CameraAccessException e) {
             Toast.makeText(this,"couldn't open camera",Toast.LENGTH_SHORT).show();
             throw new RuntimeException(e);
@@ -177,13 +229,74 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         }
     };
     public void takePreview() throws CameraAccessException {
-        Log.d(TAG,"iris takePreview");
         Surface previewSurface = mSurfaceHolder.getSurface();
-        //Surface imageSurface = mImageReader.getSurface();
+        Surface imageSurface = mImageReader.getSurface();
 
         mCaptReqBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
         mCaptReqBuilder.addTarget(previewSurface);
-        mCameraDevice.createCaptureSession(Arrays.asList(previewSurface),mSessionPreviewStateCallback, mHandler);
+        mCameraDevice.createCaptureSession(Arrays.asList(previewSurface,imageSurface),mSessionPreviewStateCallback, mHandler);
+    }
+
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Log.d(TAG,"iris onImageAvailable");
+
+            Image image = null;
+            image=reader.acquireLatestImage();  //[CHECK] reader.acquireLastImage()
+            ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[buffer.capacity()];
+            buffer.get(bytes);
+            try {
+                save(bytes);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }finally {
+                if(image!=null){
+                    image.close();
+                }
+            }
+        }
+    };
+    private void save(byte[] bytes) throws IOException{
+        Log.d(TAG,"iris save");
+        OutputStream outputStream=null;
+        try{
+            Log.d(TAG,"iris save-try");
+            outputStream=new FileOutputStream(file);
+            Log.d(TAG,"iris save-try2");
+            outputStream.write(bytes);
+            Log.d(TAG,"iris save-try3");
+            Toast.makeText(this,"274: saved image",Toast.LENGTH_SHORT).show();
+        }finally {
+            if (null != outputStream){
+                outputStream.close();
+            }
+        }
+    }
+    private void takePicture() throws CameraAccessException{
+        Log.d(TAG,"iris takePicture");
+
+        try {
+            // This is the CaptureRequest.Builder that we use to take a picture.
+            CaptureRequest.Builder cCaptReqBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            cCaptReqBuilder.addTarget(mImageReader.getSurface());
+
+            Long timeStampLong= System.currentTimeMillis()/1000;
+            String ts=timeStampLong.toString();
+            file = new File("/storage/self/primary/Pictures/irisPic"+"/"+ts+".jpg");
+
+            // Use the same AE and AF modes as the preview.
+            cCaptReqBuilder.set(CaptureRequest.CONTROL_AF_MODE,CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+            cCaptReqBuilder.set(CaptureRequest.CONTROL_AE_MODE,CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+            CaptureRequest mCaptureRequest = cCaptReqBuilder.build();
+            mSession.capture(mCaptureRequest,mSessionCaptureCallback,mainHandler);
+
+        } catch (CameraAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public CameraCaptureSession.StateCallback mSessionPreviewStateCallback = new CameraCaptureSession.StateCallback() {
@@ -204,6 +317,26 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
             Toast.makeText(null,"Configure camera failed",Toast.LENGTH_SHORT).show();
         }
     };
+    private CameraCaptureSession.CaptureCallback mSessionCaptureCallback= new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureProgressed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureResult partialResult) {
+            super.onCaptureProgressed(session, request, partialResult);
+            Log.d(TAG,"iris onCaptureProgressed");
+            mSession =session;
+        }
+
+        @Override
+        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+            Log.d(TAG,"iris onCaptureCompleted");
+            mSession=session;
+        }
+
+        @Override
+        public void onCaptureFailed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureFailure failure) {
+            super.onCaptureFailed(session, request, failure);
+        }
+    };
 
     private void setAspectRatioView(int width, int height){
         if(width>height){
@@ -219,4 +352,26 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
             mSurfaceView.setLayoutParams(new FrameLayout.LayoutParams(newWidth,newHeight));
         }
     }
+
+
+
+
+
+    private class SaveImageTask extends AsyncTask<Bitmap,Void,Void> {
+        @Override
+        protected void onPostExecute(Void aVoid){
+            super.onPostExecute(aVoid);
+            Log.d(TAG,"iris onPostExecute");
+
+            Toast.makeText(MainActivity.this,"saved Image.",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(Bitmap... bitmaps) {
+            Bitmap bitmap = null;
+            insertImage(getContentResolver(),bitmap,""+System.currentTimeMillis(),"");
+            return null;
+        }
+    }
+
 }
